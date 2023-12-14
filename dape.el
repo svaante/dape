81ebf9766a095d7ee7440a3507a7023955faf8d5
Daniel Pettersson <daniel@dpettersson.net>
2023-12-14 22:03:07 +0100

    Remove tramp prefix from find-file functions

--- dape.el
+++ dape.el
@@ -648,14 +648,18 @@ On SKIP-TRAMP-TRIM tramp prefix is keept in path."
 (defun dape-find-file (&optional default)
   "Read filename without any ignored extensions at project root.
 DEFAULT specifies which file to return on empty input."
-  (let ((completion-ignored-extensions nil)
-        (default-directory (funcall dape-cwd-fn)))
-    (expand-file-name
-     (read-file-name (if default
-                         (format "Program (default %s): " default)
-                       "Program: ")
-                     default-directory
-                     default t))))
+  (let* ((completion-ignored-extensions nil)
+         (default-directory (funcall dape-cwd-fn t))
+         (file
+          (expand-file-name
+           (read-file-name (if default
+                               (format "Program (default %s): " default)
+                             "Program: ")
+                           default-directory
+                           default t))))
+    (if (tramp-tramp-file-p file)
+        (tramp-file-name-localname (tramp-dissect-file-name file))
+      file)))
 
 (defun dape-find-file-buffer-default ()
   "Read filename at project root, defaulting to current buffer."
