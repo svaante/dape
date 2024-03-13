124193543972097651474e38e6d3447a25f2ce3a
Daniel Pettersson <daniel@dpettersson.net>
2024-03-13 11:36:37 +0100

    Update commentary section

--- dape.el
+++ dape.el
@@ -27,14 +27,18 @@
 
 ;;; Commentary:
 
-;; This package is an debug adapter client for Emacs.
-;; Use `dape-configs' to set up your debug adapter configurations.
+;; Dape is a debug adapter client for Emacs.  The debug adapter
+;; protocol, much like its more well-known counterpart, the language
+;; server protocol, aims to establish a common API for programming
+;; tools.  However, instead of functionalities such as code
+;; completions, it provides a standardized interface for debuggers.
 
-;; To initiate debugging sessions, use the command `dape'.
+;; To begin a debugging session, invoke the `dape' command.  In the
+;; minibuffer prompt, enter a debug adapter configuration name from
+;; `dape-configs'.
 
-;; Note:
-;; For complete functionality, it's essential to activate `eldoc-mode'
-;; in your source buffers and enable `repeat-mode' for ergonomics
+; For complete functionality, make sure to enable `eldoc-mode' in your
+; source buffers and `repeat-mode' for more pleasant key mappings.
 
 ;; Package looks is heavily inspired by gdb-mi.el
 
