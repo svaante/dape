f91f58849e7f4a76e0dada65908bcb8dfd26abea
Daniel Pettersson <daniel@dpettersson.net>
2025-03-30 20:27:16 +0200

    Fix Restart in mode line menu

--- dape.el
+++ dape.el
@@ -5419,7 +5419,7 @@ See `eldoc-documentation-functions', for more information."
     ["Step in" dape-step-in :enable (dape--live-connection 'stopped)]
     ["Step out" dape-step-out :enable (dape--live-connection 'stopped)]
     ["Pause" dape-pause :enable (not (dape--live-connection 'stopped t))]
-    ["Restart" dape-request]
+    ["Restart" dape-restart]
     ["Quit" dape-quit]
     "--"
     ["REPL" dape-repl]
