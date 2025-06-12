5be4ec7a0075452c4ca3305cf57cb13a98408482
Daniel Pettersson <daniel@dpettersson.net>
2025-06-12 23:59:56 +0200

    Revert back to using "python" for debugpy command

--- dape.el
+++ dape.el
@@ -171,7 +171,7 @@
                            (unless (zerop (process-file-shell-command
                                            (format "%s -c \"import debugpy.adapter\"" python)))
                              (user-error "%s module debugpy is not installed" python))))
-                command (progn (require 'python) python-interpreter)
+                command "python"
                 command-args ("-m" "debugpy.adapter" "--host" "0.0.0.0" "--port" :autoport)
                 port :autoport
                 :request "launch"
