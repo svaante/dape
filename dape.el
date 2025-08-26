484ce0303f0836b6471c2aea8c02514a959621ac
Daniel Pettersson <daniel@dpettersson.net>
2025-08-26 22:24:13 +0200

    Cosmetic change

--- dape.el
+++ dape.el
@@ -1429,9 +1429,8 @@ See `dape--connection-selected'."
       message
     (with-slots (last-id n-sent-notifs) conn
       (cond ((eq subtype 'notification)
-             (cl-incf n-sent-notifs)
              `( :type "event"
-                :seq ,(+ last-id n-sent-notifs)
+                :seq ,(+ last-id (cl-incf n-sent-notifs))
                 :event ,method
                 :body ,params))
             ((eq subtype 'request)
