324f167577aa2534fb6779e567e2cad1599c3dd3
Daniel Pettersson <daniel@dpettersson.net>
2026-02-02 21:25:45 +0100

    Bump copyright year

--- dape.el
+++ dape.el
@@ -1,6 +1,6 @@
 ;;; dape.el --- Debug Adapter Protocol for Emacs -*- lexical-binding: t -*-
 
-;; Copyright (C) 2023-2025 Free Software Foundation, Inc.
+;; Copyright (C) 2023-2026 Free Software Foundation, Inc.
 
 ;; Author: Daniel Pettersson
 ;; Maintainer: Daniel Pettersson <daniel@dpettersson.net>
